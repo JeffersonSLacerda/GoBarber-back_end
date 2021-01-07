1437a80674c18f893dee33bb6980f46f99b7c5e4
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2021-01-07 10:59:29 -0300

    :sparkles: Finish Back-end project

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,27 +1,45 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import enshureAuthenticated from '../middlewares/enshureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  user.password = 'password';
+
+  return res.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  enshureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      userId: req.user.id,
+      avatarFilename: req.file.filename,
     });
 
-    user.password = 'password';
-
     return res.json(user);
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
-});
+  },
+);
 
 export default usersRouter;
